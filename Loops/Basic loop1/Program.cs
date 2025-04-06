7e6f45299174134b06b571bd0cf1ba2ec6c88f78
mskumar05 <msk05@myyahoo.com>
2025-04-06 17:59:27 -0400

    Add files via upload

---
+++ Loops/Basic loop1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Basic_loop1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for(int i=5;i<=5;i++)
+        Console.WriteLine(i);
+    }
+}
+
---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,213 @@
+﻿/*namespace Loops;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            if (i <= 5)
+            {
+                Console.WriteLine(i);// to print vertical each number
+            }
+            else
+            {
+                Console.Write(i + " ");//to print horizontal with space
+            }
+        }
+        Console.ReadLine();
+    }
+}
+
+//
+o/p
+1
+2
+3
+4
+5
+namespace Loops;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 5; i++)
+            Console.WriteLine(i);
+        Console.ReadLine();
+    }
+}
+
+//1
+//3
+//5
+//7
+//9
+namespace loops;
+class Program
+{
+    static void Main(string[]args)
+    {
+        for (int i=1; i<=10; i++)
+        {
+            Console.WriteLine(i++);
+        }
+        Console.ReadLine();
+    }
+}
+
+//10
+//8
+//6
+//4
+//2
+//0
+namespace loops;
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i=10; i>=0; i--)
+        {
+            Console.WriteLine(i--);
+        }
+        Console.ReadLine();
+    }
+}
+
+// o/p: 1,2,3,4,5.
+namespace loops;
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i=1; i<=5; i++)
+        {
+            if (i <= 4)
+                Console.Write(i + ",");
+            else
+                Console.Write(i + ".");
+        }
+   
+        Console.ReadLine();
+    }
+}
+
+// o/p: 5,4,3,2,1.
+namespace loops;
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 5; i >= 1; i--)
+        {
+            if (i >= 2)
+                Console.Write(i + ",");
+            else
+                Console.Write(i + ".");
+        }
+
+        Console.ReadLine();
+    }
+}
+//o/p:
+//1 2 3 4 5
+//6
+  7
+  8
+  9
+  10
+
+namespace loops;
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            if (i <= 4)
+                Console.Write(i + " ");
+            else
+                Console.WriteLine(i);
+        }
+
+        Console.ReadLine();
+    }
+}
+
+O/P
+
+//10
+//9
+//8
+//7
+//6
+//5 4 3 2 1
+       
+namespace loops;
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 10; i >= 1; i--)
+        {
+            if (i >= 6)
+                Console.WriteLine(i);
+            else
+                Console.Write(i + " ");
+        }
+
+        Console.ReadLine();
+    }
+}
+
+//1 2 3 4 5
+//10
+//9
+//8
+//7
+//6
+
+namespace loops;
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+                Console.Write(i + " ");
+        }
+        Console.WriteLine();
+        for (int i = 10; i >= 6; i--)
+        {
+                Console.WriteLine(i);
+        }
+        Console.ReadLine();
+    }
+}
+
+
+//5 4 3 2 1
+//6
+//7
+//8
+//9
+//10
+namespace loops;
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 5; i >= 1; i--)
+        {
+                Console.Write(i + " ");
+        }
+        Console.WriteLine();
+        for (int i = 6; i <= 10; i++)
+        {
+                Console.WriteLine(i);
+        }
+        Console.ReadLine();
+    }
+}
+*/
